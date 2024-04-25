17c9e621fabfc6bccbd2a2e9f633f81dfea71508
stef359 <stefanossavv1@gmail.com>
2024-04-25 13:47:58 -0400

    Login JSX and its CSS

--- foodie-file-client/src/App.js
+++ foodie-file-client/src/App.js
@@ -1,6 +1,10 @@
 import "./styles/App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./components/Dashboard/Dashboard";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Navbar from "./components/Navbar/Navbar";
@@ -13,18 +17,27 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/ingredients" element={<Ingredients />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
+        <Main />
       </Router>
     </div>
+  );
+}
+
+function Main() {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname !== "/" && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/ingredients" element={<Ingredients />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </>
   );
 }
 
--- foodie-file-client/src/components/Login/Login.jsx
+++ foodie-file-client/src/components/Login/Login.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import CookingVideo from "../../assets/cooking-video.mp4";
 import "./Login.css";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="split-screen">
+      <div className="left-side">
+        <video width="90%" height="90%" autoPlay muted loop>
+          <source src={CookingVideo} type="video/mp4" />
+        </video>
+        <div id="text-overlay">
+          <h3>Where delicious recipes await</h3>
+        </div>
+      </div>
+
+      <div className="right-side">
+        <form id="form" className="login-form">
+          {" "}
+          <h2>Log In</h2>
+          <div className="form-part">
+            <label htmlFor="Username">Username</label>
+            <input type="text" id="Username" />
+          </div>
+          <div className="form-part">
+            <label htmlFor="Password">Password</label>
+            <input type="password" id="Password" />
+          </div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          <div className="help-actions"></div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
