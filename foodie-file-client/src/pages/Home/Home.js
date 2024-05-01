2b7a5f8f38f161f05e29e8094356b93f0287872b
stef359 <stefanossavv1@gmail.com>
2024-04-30 22:54:53 -0400

    home page navigation

--- foodie-file-client/src/pages/Home/Home.js
+++ foodie-file-client/src/pages/Home/Home.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const navigateToIngredients = () => navigate("/ingredients");
+  const navigateToRecipes = () => navigate("/recipes");
+
   return (
     <div className="home-container">
-      <button className="home-button" id="button1">
-        Ingredients
+      <button
+        className="home-button"
+        id="button1"
+        onClick={navigateToIngredients}
+      >
+        <em>get</em>Ingredients
       </button>
-      <button className="home-button" id="button2">
-        Recipes
+      <button className="home-button" id="button2" onClick={navigateToRecipes}>
+        <em>get</em>Recipes
       </button>
     </div>
   );
