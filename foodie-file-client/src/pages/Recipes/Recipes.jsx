585ac580ee378169adbe4d5f85c53a13c0acddc1
stef359 <stefanossavv1@gmail.com>
2024-05-01 18:52:23 -0400

    Recipes page

--- foodie-file-client/src/pages/Recipes/Recipes.jsx
+++ foodie-file-client/src/pages/Recipes/Recipes.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import SearchBar from "../../components/Search-Bar/SearchBar";
+import "./Recipes.css";
 
 const Recipes = () => {
-  return <div></div>;
+  return (
+    <div className="page">
+      <div className="input-field">
+        <SearchBar />
+        <div>Input Results</div>
+      </div>
+    </div>
+  );
 };
 
 export default Recipes;
