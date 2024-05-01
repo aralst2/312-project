90fbf8d78a079413984edd871a9a8f8b5f659f21
stef359 <stefanossavv1@gmail.com>
2024-04-30 20:49:01 -0400

    Remove Recipes and Ingridients pages

--- foodie-file-client/src/App.js
+++ foodie-file-client/src/App.js
@@ -9,8 +9,6 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
-import Recipes from "./pages/Recipes";
-import Ingredients from "./pages/Ingredients";
 import Account from "./pages/Account";
 
 function App() {
@@ -35,8 +33,6 @@ function Main() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/ingredients" element={<Ingredients />} />
         <Route path="/account" element={<Account />} />
       </Routes>
     </>
